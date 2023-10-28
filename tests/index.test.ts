88a49408b7c7cfd55589516c7d2d37f95f6c91c9
Samuel Holmes <sam@edge.app>
2023-10-27 17:58:30 -0700

    Add effects test suite

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -99,3 +99,39 @@ describe('A computed signal', () => {
     expect(message()).toBe(`Hello, World`)
   })
 })
+
+describe('Effects', () => {
+  test("should respond to signal changes", () => {
+    const log: number[] = []
+    const count = on(0)
+    const effect = on(() => {
+      log.push(count())
+    })
+
+    // Activate the effect
+    effect()
+
+    count(1)(2)(3)
+
+    expect(log).toEqual([0, 1, 2, 3])
+  })
+
+  test("should respond to deeper signal changes", () => {
+    const log: number[] = []
+    const count = on(0)
+    const square = on(() => {
+      const out = count() ** 2
+      return out
+    })
+    const effect = on(() => {
+      log.push(square())
+    })
+
+    // Activate the effect
+    effect()
+
+    count(1)(2)(3)
+
+    expect(log).toEqual([0, 1, 4, 9])
+  })
+})
