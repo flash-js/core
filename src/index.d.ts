b795054d31af8f7848396fbdf7c82c2905d3b454
Samuel Holmes <sam@edge.app>
2023-11-26 20:08:10 -0800

    Rename SignalComputation type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,3 @@
-export type SignalComputation<T, Args extends any[]> = (...args: [...Args]) => T
-
 export interface StaticSignal<T>{
    (): T 
    (...value: T[]): StaticSignal<T> 
@@ -9,5 +7,7 @@ export interface ComputedSignal<T, Args extends any[] = any[] >{
    (...args: [...Args]): ComputedSignal<T>
 }
 
-export declare function on<T, Args extends any[] = any[] >(compute: SignalComputation<T, Args>): ComputedSignal<T, Args>
+export type ComputeFn<T, Args extends any[]> = (...args: [...Args]) => T
+
+export declare function on<T, Args extends any[] = any[] >(compute: ComputeFn<T, Args>): ComputedSignal<T, Args>
 export declare function on<T>(initialValue?: T): StaticSignal<T>;
